c5e8eaf2325bf646e7e8fb73484ae26b8283b24b
RedCode <dannegru40@gmail.com>
2021-02-03 19:01:03 +0200

    using env variables

---
+++ src/Firebase_Config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    databaseURL: process.env.REACT_APP_databaseURL,
+  };
+export default config
