c868ca61f9c80bf32538bd3f3dddf804b145825d
White Red <rednblkx@protonmail.com>
2021-02-03 02:52:35 +0200

    using firebase

--- src/AppInfo.js
+++ src/AppInfo.js
@@ -1,7 +1,8 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, makeStyles, Modal, Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { Accordion, AccordionDetails, AccordionSummary, Box, CircularProgress, IconButton, makeStyles, Modal, Paper, Typography, useMediaQuery } from "@material-ui/core";
 import AppLogo from './ic_launcher.png';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { CloseOutlined } from "@material-ui/icons";
+import { FirestoreCollection, FirestoreDocument } from "@react-firebase/firestore";
 
 const useStyles = makeStyles((theme) => ({
     containerLg: {
@@ -26,10 +27,13 @@ const useStyles = makeStyles((theme) => ({
     },
     accordionDetails: {
         flexDirection: 'column'
-    }
+    },
+    loading: {
+        display: "inline-flex"
+      }
   }));
 
-export default function AppInfo({history}){
+export default function AppInfo({history, match}){
   const classes = useStyles();
   const matches = useMediaQuery('(min-width:600px)');
   return(
@@ -38,11 +42,14 @@ export default function AppInfo({history}){
         onClose={() => history.goBack()}
         className={classes.containerLg}
     >
-        <Paper className={classes.content}>
+        <FirestoreDocument path={`/apps/${match.params.appId}`}>
+        {d =><Paper className={classes.content}>
             <IconButton style={{float: "right"}} onClick={() => history.goBack()}><CloseOutlined/></IconButton>
             <Box textAlign="center" p={"2rem"}>
                 <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography  variant={matches ? "h2" : "h4"}>Cluedo sheet & share</Typography>
+                {d.isLoading === undefined || d.isLoading === true ? <CircularProgress color="primary" size="5rem" classes={{root: classes.loading}}/> : 
+                <Typography  variant={matches ? "h2" : "h4"}>{d.value.name}</Typography>}
+                {(d.isLoading !== undefined || d.isLoading !== false) && d.value && d.value.playstore ? <a href={d.value.url}><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' width="200px"/></a> : null}
             </Box>
             <Accordion classes={{expanded: classes.accordion, root: classes.accordion}}>
                 <AccordionSummary
@@ -53,58 +60,46 @@ export default function AppInfo({history}){
                     <Typography>About</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
+                {d.isLoading === undefined || d.isLoading === true ? <CircularProgress color="primary" size="5rem" classes={{root: classes.loading}}/> : 
                     <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
+                        {d.value.about || "No description available"}
+                    </Typography>}
                 </AccordionDetails>
             </Accordion>
-            <Accordion classes={{expanded: classes.accordion, root: classes.accordion}}>
-                <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                >
-                    <Typography>FAQ</Typography>
-                </AccordionSummary>
-                <AccordionDetails classes={{root: classes.accordionDetails}}>
-                    {/* <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography> */}
-                    <Accordion>
-                        <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                        >
-                            <Typography>What's goin' on?</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                                sit amet blandit leo lobortis eget.
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>                        
-                    <Accordion>
-                        <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                        >
-                            <Typography>Sharing does not work</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                                sit amet blandit leo lobortis eget.
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>
-                </AccordionDetails>
+                 <Accordion classes={{expanded: classes.accordion, root: classes.accordion}}>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                        <Typography>FAQ</Typography>
+                    </AccordionSummary>
+                <FirestoreCollection path={`/apps/${match.params.appId}/faq`}>
+                    {c => c.isLoading === undefined || c.isLoading === true ? <CircularProgress color="primary" size="5rem" classes={{root: classes.loading}}/> :
+                    <AccordionDetails classes={{root: classes.accordionDetails}}>
+                        {/* <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                            sit amet blandit leo lobortis eget.
+                        </Typography> */}
+                        {c.value.lenght > 0 ? c.value.map(f => <Accordion>
+                            <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                            >
+                                <Typography>{f.question}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    {f.answer}
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>) : <Typography variant="h4">No FAQs available</Typography>}   
+                    </AccordionDetails>}
+                </FirestoreCollection>
             </Accordion>
-        </Paper>
+        </Paper>}
+        </FirestoreDocument>
     </Modal>
   )
 }
--- src/AppList.js
+++ src/AppList.js
@@ -1,7 +1,12 @@
-import { Container, Grid, makeStyles, Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { CircularProgress, Container, Grid, makeStyles, Paper, Typography, useMediaQuery } from "@material-ui/core";
 import { useRouteMatch, Link as RouterLink, Switch, Route, withRouter } from "react-router-dom";
 import AppLogo from './ic_launcher.png';
 import AppInfo from './AppInfo';
+import configFirebase from './Firebase_Config';
+import { FirestoreCollection, FirestoreProvider } from "@react-firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import { Component } from "react";
 
 const useStyles = makeStyles((theme) => ({
     containerLg: {
@@ -11,7 +16,8 @@ const useStyles = makeStyles((theme) => ({
       // justifyContent: "center",
       // alignItems: "center",
       // marginBottom: "2rem",
-      marginTop: "2rem"
+      marginTop: "2rem",
+      textAlign: "center"
     },    
     containerSm: {
       // height: "100vh",
@@ -20,7 +26,8 @@ const useStyles = makeStyles((theme) => ({
       // justifyContent: "center",
       // alignItems: "center",
       // marginBottom: "2rem",
-      marginTop: "2rem"
+      marginTop: "2rem",
+      textAlign: "center"
     },
     textheading: {
       paddingBottom: "3rem",
@@ -35,6 +42,9 @@ const useStyles = makeStyles((theme) => ({
       height: "fit-content",
       width: "153px",
       textAlign: "center"
+    },
+    loading: {
+      display: "inline-flex"
     }
   }));
 
@@ -44,53 +54,31 @@ export default function AppList(){
   let match = useRouteMatch();
   const AppIRoute = withRouter(AppInfo);
   return(
+    <FirestoreProvider {...configFirebase} firebase={firebase}>
+    <FirestoreCollection path="/apps/" orderBy={{field: "index", type: "asc"}}>
+      {d => 
       <Container elevation={4} className={matches ? classes.containerLg : classes.containerSm}>
-          <Typography variant={matches ? "h2" : "h4"} align="center" className={classes.textheading}>
-          Our Apps/Games
-          </Typography>
+        <Typography variant={matches ? "h2" : "h4"} align="center" className={classes.textheading}>
+        Our Apps/Games
+        </Typography>
+        {d.isLoading ? <CircularProgress color="primary" size="5rem" classes={{root: classes.loading}}/> : 
           <Grid container spacing={3} justify="center">
-          <Grid item component={RouterLink} to={`${match.url}/1`} style={{textDecoration: "none"}}>
+            {d.value.map((app, i) => (
+            <Grid item key={i} component={RouterLink} to={`${match.url}/${d.ids[i]}`} style={{textDecoration: "none"}}>
               <Paper className={classes.paper} >
-                <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography paragraph align="center">Clueshare</Typography>
+              <img src={AppLogo} width="100px" alt={app.name}/>
+                <Typography paragraph align="center">{app.name}</Typography>
               </Paper>
-          </Grid>
-          <Grid item component={RouterLink} to={`${match.url}/2`} style={{textDecoration: "none"}}>
-              <Paper className={classes.paper}>
-                <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography paragraph align="center">Lorem ipsum dolor</Typography>
-              </Paper>
-          </Grid>
-          <Grid item component={RouterLink} to={`${match.url}/3`} style={{textDecoration: "none"}}>
-              <Paper className={classes.paper}>
-                <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography paragraph align="center">sit amet, consecteur</Typography>
-              </Paper>
-          </Grid>            
-          <Grid item component={RouterLink} to={`${match.url}/4`} style={{textDecoration: "none"}}>
-              <Paper className={classes.paper}>
-                <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography paragraph align="center">adipiscing elit</Typography>
-              </Paper>
-          </Grid>
-          <Grid item component={RouterLink} to={`${match.url}/5`} style={{textDecoration: "none"}}>
-              <Paper className={classes.paper}>
-                <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography paragraph align="center">, sed do eiusmod</Typography>
-              </Paper>
-          </Grid>
-          <Grid item component={RouterLink} to={`${match.url}/6`} style={{textDecoration: "none"}}>
-              <Paper className={classes.paper}>
-                <img src={AppLogo} width="100px" alt="Clueshare "/>
-                <Typography paragraph align="center">tempor incididunt</Typography>
-              </Paper>
-          </Grid>
-          </Grid>
+            </Grid>))}
+          </Grid>}
           <Switch>
             <Route path={`${match.path}/:appId`} exact>
               <AppIRoute/>
             </Route>
           </Switch>
       </Container>
+      }
+    </FirestoreCollection>
+      </FirestoreProvider>
   )
 }
